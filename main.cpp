7d3fc01bd12f8f1f2f22a63fb8b0dc95816d758d
Houtje <abyssalwhip@live.nl>
2016-11-15 14:16:44 +0100

    Commit 2
    plaatje in vlakken

--- main.cpp
+++ main.cpp
@@ -3,9 +3,18 @@
 #include <QWidget>
 #include <QStandardItemModel>
 #include <QTableView>
+#include <QImage>
+#include <QTextStream>
+#include <iostream>
+#include <sstream>
+#include <string>
 
-#define WIDTH_DEF 400
-#define LENGTH_DEF 300
+QString ItoS ( int number )
+{
+  std::ostringstream oss;
+  oss << number;
+  return QString::fromStdString(oss.str());
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,17 +24,27 @@ int main(int argc, char *argv[])
     QStandardItemModel *model = new QStandardItemModel(4,3);
     for (int row = 0; row < 4; ++row) {
         for (int column = 0; column < 3; ++column) {
-            QStandardItem *item = new QStandardItem(QString("row %0, column %1").arg(row).arg(column));
+            QString str = "C:/Users/Joris/Documents/Puzzling_Collections/images/_";
+            str += ItoS(row);
+            str += "_";
+            str += ItoS(column);
+            str += ".jpeg";
+            QImage image(str);
+            QStandardItem *item = new QStandardItem();
+            item->setData(QVariant(QPixmap::fromImage(image)), Qt::DecorationRole);
+            //item->setSizeHint(QSize(100,100));
             model->setItem(row, column, item);
         }
     }
 
     QTableView *table = new QTableView(widget);
-    table->setFixedSize(WIDTH_DEF,LENGTH_DEF);
     table->setModel(model);
+    table->resizeColumnsToContents();
+    table->resizeRowsToContents();
+    table->resize(1920,1080);
 
     widget->setWindowTitle("Puzzling Collections");
-    table->setFixedSize(WIDTH_DEF,LENGTH_DEF);
+    widget->resize(1920,1080);
     widget->show();
 
     return a.exec();
